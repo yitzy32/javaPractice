a37e88bd840bf4a49b3659b23c2d7f9fe0b5af99
yitzy32 <yitzybratt32@gmail.com>
2021-12-22 14:14:28 -0500

    show a frame

---
+++ src/ShowAFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class ShowAFrame {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setSize(300, 300);
+    frame.setTitle("This is frame");
+    frame.setVisible(true);
+  }
+}
